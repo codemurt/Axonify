f08dd773ec6274bca22b03eba2ab8844d5be611c
Matthew <sarapulov.03@mail.ru>
2021-12-16 15:03:38 +0500

    Small fixes

--- pages.py
+++ pages.py
@@ -3,10 +3,30 @@ import os
 import sys
 import tkinter as tk
 import tkinter.ttk as ttk
-from time import sleep
 from tkinter.filedialog import askdirectory
 
-import train
+
+# import train
+
+def get_datasets():
+    out = ()
+    if not os.path.exists("Resources/datasets.txt"):
+        with open("Resources/datasets.txt", "w") as f:
+            f.write("")
+    datasets_file = open("Resources/datasets.txt", 'r')
+    global datasets
+    datasets = datasets_file.readlines()
+    datasets_file.close()
+    for ds in datasets:
+        if len(ds) > 0:
+            ds_name = ds.split()[0]
+            out = out.__add__((str(ds_name),))
+    return out
+
+
+def get_datasets_with_new():
+    out = get_datasets()
+    return out.__add__(("Create new dataset...",))
 
 
 class StartPage(tk.Frame):
@@ -24,25 +44,27 @@ class GeneratorPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        label = tk.Label(self, text="Select the number of images to generate:", font=controller.title_font)
+        label = tk.Label(self, text="Select dataset and number of images to generate:", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
-        inp = tk.Entry(self)
-        inp.pack()
+        dataset_selector = ttk.Combobox(self, values=get_datasets(), justify=tk.CENTER)
+        dataset_selector.pack()
+        epoch_count = tk.Entry(self)
+        epoch_count.pack()
 
         def generate_pics(imgCount):
-            if imgCount.isdigit():
+            if imgCount.isdigit() and not dataset_selector.current() == -1:
                 print("WIP")
                 #
                 # do stuff
                 #
-                inp.delete(0, 'end')
+                epoch_count.delete(0, 'end')
                 controller.show_frame("GenerationEnd")
 
         def show_start():
-            inp.delete(0, 'end')
+            epoch_count.delete(0, 'end')
             controller.show_frame("StartPage")
 
-        tk.Button(self, text="Generate", command=lambda: generate_pics(inp.get())).pack()
+        tk.Button(self, text="Generate", command=lambda: generate_pics(epoch_count.get())).pack()
         tk.Button(self, text="Back", command=show_start).pack()
 
 
@@ -60,32 +82,16 @@ class DatasetSelection(tk.Frame):
         self.controller = controller
         tk.Label(self, text='Select dataset:').pack()
 
-        def get_datasets():
-            print("WIP")
-            out = ()
-            if not os.path.exists("Resources/datasets.txt"):
-                with open("Resources/datasets.txt", "w") as f:
-                    f.write("")
-            datasets_file = open("Resources/datasets.txt", 'r')
-            global datasets
-            datasets = datasets_file.readlines()
-            datasets_file.close()
-            for ds in datasets:
-                ds_name = ds.split()[0]
-                out = out.__add__((str(ds_name),))
-            out = out.__add__(("Create new dataset...",))
-            return out
-
-        dataset = ttk.Combobox(self, values=get_datasets(), justify=tk.CENTER)
-        dataset.current(0)
-        dataset.pack()
+        datasets_selector = ttk.Combobox(self, values=get_datasets_with_new(), justify=tk.CENTER)
+        datasets_selector.current(0)
+        datasets_selector.pack()
 
         def next_frame():
-            if dataset.current() == len(dataset["values"]) - 1:
+            if datasets_selector.current() == len(datasets_selector["values"]) - 1:
                 controller.show_frame("DatasetCreation")
             else:
                 global datasetName
-                datasetName = dataset.get()
+                datasetName = datasets_selector.get()
                 for i in datasets:
                     if i.split()[0] == datasetName:
                         global datasetDirectory
@@ -95,7 +101,7 @@ class DatasetSelection(tk.Frame):
         tk.Button(self, text='Next', command=next_frame).pack()
 
         def show_start():
-            dataset.current(0)
+            datasets_selector.current(0)
             controller.show_frame("StartPage")
 
         tk.Button(self, text="Back", command=show_start).pack()
@@ -118,13 +124,14 @@ class DatasetCreation(tk.Frame):
 
         def create_start_train(name):
             if self.pathToNewSet != "" and name != "":
-                print("WIP", self.pathToNewSet, name)
+                name_label.delete(0, 'end')
+                print(self.pathToNewSet, name)
                 global datasetName
                 global datasetDirectory
                 datasetName = name
                 datasetDirectory = self.pathToNewSet
                 datasets_file = open("Resources/datasets.txt", 'a')
-                datasets_file.write('\n' + datasetName + " " + datasetDirectory)
+                datasets_file.write(datasetName + " " + datasetDirectory + '\n')
                 datasets_file.close()
                 controller.show_frame("ChooseEpochs")
 
@@ -142,7 +149,6 @@ class ChooseEpochs(tk.Frame):
 
         def set_epochs(epochCount):
             if epochCount.isdigit():
-                print("WIP")
                 global epochCounter
                 epochCounter = int(epochCount)
                 inp.delete(0, 'end')
@@ -155,11 +161,9 @@ class Training(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        cur_ep = tk.Label(self, text='Current epoch: 0/' + str(epochCounter))
 
         def start():
-            cur_ep.pack()
-            train.main(epochCounter, datasetDirectory)
+            # train.main(epochCounter, datasetDirectory)
             controller.show_frame("TrainingEnd")
 
         tk.Button(self, text="Start training", command=start).pack()

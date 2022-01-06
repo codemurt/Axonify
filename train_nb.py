fe687bed21f31bdfb70388b68e6dae31d94a59b7
codemurt <egor.lebe@inbox.ru>
2022-01-06 14:56:30 +0500

    changed arg for parser

--- train_nb.py
+++ train_nb.py
@@ -28,7 +28,7 @@ parser.add_argument('--ndf', type=int, default=64)
 parser.add_argument('--epochs', type=int, default=25, help='number of epochs to train for')
 parser.add_argument('--lr', type=float, default=0.002, help='learning rate, default=0.0002')
 parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
-parser.add_argument('--isTrainMode', required=True, help='is train mode, if not will be create dataset mode')
+parser.add_argument('--isTrainMode', type=int, required=True, default=1, help='is train mode, if not will be create dataset mode')
 parser.add_argument('--datasetName', help='the name for a new dataset')
 parser.add_argument('--ngpu', type=int, default=1, help='number of GPUs to use')
 parser.add_argument('--outf', default='.', help='folder to output images and model checkpoints')
@@ -38,7 +38,7 @@ opt = parser.parse_args()
 print(opt)
 path_to_dataset = opt.outf + '/' + opt.datasetName
 
-if not opt.IsTrainMode:
+if opt.IsTrainMode == 0:
     try:
         os.makedirs(opt.outf)
     except OSError:

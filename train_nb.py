8e5175e5c0a3c0be44f640e924bceaf65956913f
Codemurt <egor.lebe@inbox.ru>
2022-01-22 16:14:31 +0500

    optimizers changes

--- train_nb.py
+++ train_nb.py
@@ -230,8 +230,8 @@ elif int(opt.mode) == 1:
         netD.load_state_dict(torch.load(pathNetD))
         print("loaded netD and optimizerD")
     
-    optimizerD = optim.Adam(netD.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
-    optimizerG = optim.Adam(netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+    optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
+    optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(beta1, 0.999))
     
     print("Start training")
     for epoch in range(num_epochs):
